0ed61fe6d29412dd32a0ca3197a83a1844e16ba7
Thibault Chardon <chardon@ipgp.fr>
2026-09-11 17:42:22 +0200

    Edited the metadata import after WFS edits from IGN

--- lidar_hd_tools/metadata.py
+++ lidar_hd_tools/metadata.py
@@ -8,35 +8,41 @@ wfs = WebFeatureService(url=wfs_url, version="2.0.0")
 
 def get_metadata(gdf):
 
-    lidar_layer = "IGNF_NUAGES-DE-POINTS-LIDAR-HD:dalle"
-    DSM_layer = "IGNF_MNS-LIDAR-HD:dalle"
-    DEM_layer = "IGNF_MNT-LIDAR-HD:dalle"
-
+    layer = "IGNF_LIDAR-HD_METADONNEE:metadata"
     bbox = gdf.to_crs("epsg:4326").union_all().bounds
 
-    merged_metadata = []
+    response = wfs.getfeature(typename=layer, outputFormat="application/json",
+                              bbox=bbox, srsname="EPSG:4326")
 
-    for layer_name in [lidar_layer, DSM_layer, DEM_layer]:
+    with io.BytesIO(response.read()) as f:
 
-        response = wfs.getfeature(typename=layer_name, outputFormat="application/json",
-                                  bbox=bbox, srsname="EPSG:4326")
+        metadata = gpd.read_file(f)
+        metadata = metadata.loc[metadata.geometry.intersects(gdf.to_crs("epsg:4326").geometry.union_all())].reset_index(drop=True)
 
-        with io.BytesIO(response.read()) as f:
 
-            metadata = gpd.read_file(f)
-            metadata = metadata.loc[metadata.geometry.intersects(gdf.to_crs("epsg:4326").geometry.union_all())].reset_index(drop=True)
+        if metadata.empty:
+            raise ValueError(f"No tile found. Please check the selected location and used CRS.")
 
-            merged_metadata.append(metadata)
+    syst_planim = np.unique(metadata["systeme_planimetrique"])[0]
 
-            if metadata.empty:
-                raise ValueError(f"No tile found. Please check the selected location and used CRS.")
+    if syst_planim == "LAMB93":
+        projection = "EPSG:2154"
+    elif syst_planim == "RGAF09UTM20":
+        projection = "EPSG:5490"
+    elif syst_planim == "RGR92UTM40S":
+        projection = "EPSG:2975"
+    elif syst_planim == "RGM23UTM38S":
+        projection = "EPSG:10674"
+    else:
+        raise ValueError(f"Unknown projection: {syst_planim}")
 
-    projection = np.unique(merged_metadata[0]["projection"])[0] # assuming only one local projection used
-    dem_tiles_urls = np.unique(merged_metadata[2]['url']).tolist()
-    dem_tiles_filenames = np.unique(merged_metadata[2]['name_download']).tolist()
-    dsm_tiles_urls = np.unique(merged_metadata[1]['url']).tolist()
-    dsm_tiles_filenames = np.unique(merged_metadata[1]['name_download']).tolist()
-    lidar_tiles_urls = np.unique(merged_metadata[0]['url']).tolist()
-    lidar_tiles_filenames = np.unique(merged_metadata[0]['name_download']).tolist()
+    dem_tiles_urls = np.unique(metadata['url_mnt']).tolist()
+    dem_tiles_filenames = [url.split("FILENAME=")[-1] for url in dem_tiles_urls]
+
+    dsm_tiles_urls = np.unique(metadata['url_mns']).tolist()
+    dsm_tiles_filenames = [url.split("FILENAME=")[-1] for url in dsm_tiles_urls]
+
+    lidar_tiles_urls = np.unique(metadata['url_npl']).tolist()
+    lidar_tiles_filenames = [url.split("/")[-1] for url in lidar_tiles_urls]
 
     return projection, (dem_tiles_urls, dsm_tiles_urls, lidar_tiles_urls), (dem_tiles_filenames, dsm_tiles_filenames, lidar_tiles_filenames)
