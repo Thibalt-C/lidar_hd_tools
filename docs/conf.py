237906fd0363c06cfd303bc7922546eaef64cdde
Thibault Chardon <62816126+Thibalt-C@users.noreply.github.com>
2026-09-22 15:17:35 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,11 @@
-master_doc = "ReadMe.md"
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../src"))
+
+project = "lidar_hd_tools"
+extensions = []
+templates_path = ["_templates"]
+exclude_patterns = []
+
+html_theme = "alabaster"
+master_doc = "index"
